dbf017772cc5000a6eb11aead2b8435ff5f94916
LegendeJJ <dalienjj@gmail.com>
2022-06-23 18:36:18 +0200

    .

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ setInterval(() => {
     }
 
 }, 4000)
+
+const menu = document.querySelector(".menu").addEventListener("click", () => {
+    document.querySelectorAll(".target").forEach((item) => {
+        item.classList.toggle("change")
+    })
+})
